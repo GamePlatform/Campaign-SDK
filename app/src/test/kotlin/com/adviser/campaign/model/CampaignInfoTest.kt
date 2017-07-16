2cbd5367ac3c8e6fac101e37348551c7295686e5
kym <rladbals0733@gmail.com>
2017-07-16 11:08:34 +0900

    Add test code and template_num -> templalte

---
+++ app/src/test/kotlin/com/adviser/campaign/model/CampaignInfoTest.kt
@@ -0,0 +1,7 @@
+package com.adviser.campaign.model
+
+/**
+ * Created by vaio on 2017-07-10.
+ */
+class CampaignInfoTest {
+}
---
+++ app/src/test/kotlin/com/adviser/campaign/model/HttpRequestAgentTest.kt
@@ -0,0 +1,7 @@
+package com.adviser.campaign.model
+
+/**
+ * Created by vaio on 2017-07-15.
+ */
+class HttpRequestAgentTest {
+}
